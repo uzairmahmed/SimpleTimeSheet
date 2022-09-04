cb06f8f74c956a7be500a6277e18978152619d29
Uzair Ahmed <uzairmahmed@gmail.com>
2022-09-04 02:46:45 -0400

    timesheet now displays on admin page

--- timesheets/src/components/admin/TimesheetChart.js
+++ timesheets/src/components/admin/TimesheetChart.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import {
   Center,
   Table, Thead, Tbody, Tfoot, Tr, Th, Td, TableContainer,
@@ -6,136 +6,61 @@ import {
 import { useTable } from "react-table";
 
 export default function TimesheetAdminChart(props) {
-  const data = React.useMemo(() => ([
+  const [tsRows, settsRows] = useState([
     {
-      "Key0": "Julian",
-      "Key1": "hhs.gov",
-      "Key2": "Teal",
-      "Key3": "Electric House, The",
-      "Key4": "Chevrolet",
-      "Key5": "DB9",
-      "Key6": "Aminophylline",
-      "Key7": "Pantoprazole Sodium",
-      "Key8": "#053e17",
-      "Key9": "Legaan, water"
+      "Key0": "Loading",
+      "Key1": "Loading",
+
     },
     {
-      "Key0": "Ketti",
-      "Key1": "desdev.cn",
-      "Key2": "Turquoise",
-      "Key3": "Liberty Heights",
-      "Key4": "Ford",
-      "Key5": "Montero Sport",
-      "Key6": "Carvedilol",
-      "Key7": "Bay Leaf",
-      "Key8": "#53bb47",
-      "Key9": "Admiral, indian red"
+      "Key0": "Loading",
+      "Key1": "Loading",
+
     },
     {
-      "Key0": "Fallon",
-      "Key1": "elegantthemes.com",
-      "Key2": "Yellow",
-      "Key3": "Exit to Hell",
-      "Key4": "Toyota",
-      "Key5": "GranSport",
-      "Key6": "OCTINOXATE, PADIMATE O",
-      "Key7": "Vita",
-      "Key8": "#bffdfa",
-      "Key9": "Brown antechinus"
+      "Key0": "Loading",
+      "Key1": "Loading",
     },
-  ]), []);
-  const columns = React.useMemo(() => (
-    [
-      {
-        "id": "columnId_00.27448239441897093",
-        "Header": "Start Date",
-        "Footer": "",
-        "columns": [
-          {
-            "id": "columnId_0_00.4200340858481082",
-            "Header": "Date",
-            "Footer": "",
-            "accessor": "Key0"
-          }
-        ]
-      },
-      {
-        "id": "columnId_00.49427072112246595",
-        "Header": "End Date",
-        "Footer": "",
-        "columns": [
-          {
-            "id": "columnId_0_10.37746792551191",
-            "Header": "Day",
-            "Footer": "",
-            "accessor": "Key1"
-          }
-        ]
-      },
-      {
-        "id": "columnId_00.18196155910161682",
-        "Header": "Name 1",
-        "Footer": "Total",
-        "columns": [
-          {
-            "id": "columnId_0_20.8006097687181224",
-            "Header": "In",
-            "Footer": "",
-            "accessor": "Key2"
-          },
-          {
-            "id": "columnId_0_20.8653883039581565",
-            "Header": "Out",
-            "Footer": "",
-            "accessor": "Key3"
-          },
-          {
-            "id": "columnId_0_20.5297450529824548",
-            "Header": "Break",
-            "Footer": "",
-            "accessor": "Key4"
-          },
-          {
-            "id": "columnId_0_20.9996184883377395",
-            "Header": "Total",
-            "Footer": "",
-            "accessor": "Key5"
-          }
-        ]
-      },
-      {
-        "id": "columnId_00.7916071055758422",
-        "Header": "Name 2",
-        "Footer": "Total",
-        "columns": [
-          {
-            "id": "columnId_0_30.8437703760600028",
-            "Header": "In",
-            "Footer": "",
-            "accessor": "Key6"
-          },
-          {
-            "id": "columnId_0_30.8102445443140622",
-            "Header": "Out",
-            "Footer": "",
-            "accessor": "Key7"
-          },
-          {
-            "id": "columnId_0_30.07666480764828454",
-            "Header": "Break",
-            "Footer": "",
-            "accessor": "Key8"
-          },
-          {
-            "id": "columnId_0_30.055637219429985674",
-            "Header": "Total",
-            "Footer": "",
-            "accessor": "Key9"
-          }
-        ]
+  ]);
+  const [tsCols, settsCols] = useState([
+    {
+      "id": "columnId_00.27448239441897093",
+      "Header": "Start Date",
+      "Footer": "",
+      "columns": [
+        {
+          "id": "columnId_0_00.4200340858481082",
+          "Header": "Date",
+          "Footer": "",
+          "accessor": "Key0"
+        }
+      ]
+    },
+    {
+      "id": "columnId_00.49427072112246595",
+      "Header": "End Date",
+      "Footer": "",
+      "columns": [
+        {
+          "id": "columnId_0_10.37746792551191",
+          "Header": "Day",
+          "Footer": "",
+          "accessor": "Key1"
+        }
+      ]
+    }
+  ]);
+
+  const data = React.useMemo(() => (tsRows), [tsRows]);
+  const columns = React.useMemo(() => (tsCols), [tsCols]);
+
+  useEffect(() => {
+      let data = props.getTSChart()
+      if (data){
+        settsRows(data.rows)
+        settsCols(data.columns)
       }
-    ]
-  ), []);
+  }, [props.refresh]);
 
   const {
     getTableProps,
@@ -144,7 +69,6 @@ export default function TimesheetAdminChart(props) {
     footerGroups,
     rows,
     prepareRow,
-
   } = useTable({ columns, data, },
     );
 
--- timesheets/src/components/admin/TimesheetView.js
+++ timesheets/src/components/admin/TimesheetView.js
@@ -8,14 +8,19 @@ import React, { useState, useEffect } from 'react'
 import TimesheetAdminChart from './TimesheetChart'
 import TimesheetCreateForm from './TimesheetCreateForm'
 
-import { getTimesheets, writeCurrentTimesheet, getCurrentTimesheet } from '../../firebase/Functions';
+import { getTimesheetData, getTimesheets, writeCurrentTimesheet, getCurrentTimesheet } from '../../firebase/Functions';
 
 export default function TimesheetView(props) {
   const [isOpen, setIsOpen] = useState(false);
   const [timesheets, setTimesheets] = useState([]);
   const [currentTS, setCurrentTS] = useState('')
   const [selectedTS, setSelectedTS] = useState('')
+  const [refresh, setRefresh] = useState(0);
 
+  const [selectedTSData, setSelectedTSData] = useState({
+    rows: [],
+    columns: []
+  })
 
   useEffect(() => {
     handleTimeSheets()
@@ -29,12 +34,157 @@ export default function TimesheetView(props) {
     setSelectedTS(currentTS)
   }
 
-  function handleTimesheetSelect(val) {
+
+  async function handleTimesheetSelect(val) {
     setSelectedTS(val)
+    const tsV = await getTimeSheetValues(val)
+    setSelectedTSData(tsV)
+    setRefresh(refresh+1)
   }
 
   async function handleSet() {
     await writeCurrentTimesheet(selectedTS)
+  }
+
+  async function getTimeSheetValues(val) {
+    if (val) {
+      let tData = await getTimesheetData(val)
+      return processTimesheet(tData)
+    }
+  }
+
+  function processTimesheet(val) {
+    let columns = [
+      {
+        "id": "columnId_00."+(Math.floor(Math.random() * 1000000000)),
+        "Header": val[0].date,
+        "Footer": "",
+        "columns": [
+          {
+            "id": "columnId_0_00."+(Math.floor(Math.random() * 1000000000)),
+            "Header": "Date",
+            "Footer": "",
+            "accessor": "Key0"
+          }
+        ]
+      }, {
+        "id": "columnId_00."+(Math.floor(Math.random() * 1000000000)),
+        "Header": val[13].date,
+        "Footer": "",
+        "columns": [
+          {
+            "id": "columnId_0_10."+(Math.floor(Math.random() * 1000000000)),
+            "Header": "Day",
+            "Footer": "",
+            "accessor": "Key1"
+          }
+        ]
+      },
+    ]
+
+    let rows = []
+
+    Object.keys(val[0]).forEach(function (emp, index) {
+      if (val[0][emp].name) {
+        columns.push({
+          "id": "columnId_00."+(Math.floor(Math.random() * 1000000000)),
+          "Header": val[0][emp].name,
+          "Footer": "Total",
+          "columns": [
+            {
+              "id": "columnId_0_20."+(Math.floor(Math.random() * 1000000000)),
+              "Header": "In",
+              "Footer": "",
+              "accessor": val[0][emp].name + "_In"
+            },
+            {
+              "id": "columnId_0_20."+(Math.floor(Math.random() * 1000000000)),
+              "Header": "Out",
+              "Footer": "",
+              "accessor": val[0][emp].name + "_Out"
+            },
+            {
+              "id": "columnId_0_20."+(Math.floor(Math.random() * 1000000000)),
+              "Header": "Break",
+              "Footer": "",
+              "accessor": val[0][emp].name + "_Break"
+            },
+            {
+              "id": "columnId_0_20."+(Math.floor(Math.random() * 1000000000)),
+              "Header": "Total",
+              "Footer": "",
+              "accessor": val[0][emp].name + "_Total"
+            }
+          ]
+        })
+      }
+    })
+
+    val.forEach(date => {
+      var options = { year: 'numeric', month: 'long', day: 'numeric' };
+      let dateText = new Date(date.date).toUTCString("en-US", options).slice(5, 16)
+      let dayText = new Date(date.date).toUTCString("en-US", options).slice(0, 3)
+      let temp = {};
+
+      temp["Key0"] = dateText
+      temp["Key1"] = dayText
+
+
+      Object.keys(val[0]).forEach(function (emp, index) {
+        if (val[0][emp].name) {
+          let IN = val[0][emp].name + "_In"
+          let OUT = val[0][emp].name + "_Out"
+          let BREAK = val[0][emp].name + "_Break"
+          let TOTAL = val[0][emp].name + "_Total"
+
+          let stime
+          let etime
+          let notes
+
+          if (date[emp].stime) {
+            stime = date[emp].stime
+          } else {
+            stime = "-"
+          }
+
+          if (date[emp].etime) {
+            etime = date[emp].etime
+          } else {
+            etime = "-"
+          }
+
+          if (date[emp].notes) {
+            notes = date[emp].notes
+          } else {
+            notes = "-"
+          }
+
+          let temp2 = {}
+
+          temp2[IN] = stime
+          temp2[OUT] = etime
+          temp2[BREAK] = "99"
+          temp2[TOTAL] = "99"
+
+          temp = {
+            ...temp,
+            ...temp2
+          }
+        }
+      })
+
+
+      rows.push(temp)
+    });
+
+    return {
+      rows: rows,
+      columns: columns
+    }
+  }
+
+  function getTSChart() {
+    return selectedTSData
   }
 
   function handleCreate() {
@@ -55,7 +205,6 @@ export default function TimesheetView(props) {
           <ModalCloseButton />
           <ModalBody>
             <TimesheetCreateForm onclose={() => onClose()} />
-            {/* <EmployeeEditForm onclose={() => onClose()} employee={temployee} /> */}
           </ModalBody>
         </ModalContent>
       </Modal>
@@ -94,7 +243,7 @@ export default function TimesheetView(props) {
           </HStack>
         </Center>
         <Center w='100%'>
-          <TimesheetAdminChart />
+          <TimesheetAdminChart getTSChart={() => getTSChart()} refresh={refresh} />
         </Center>
       </VStack>
     </>
