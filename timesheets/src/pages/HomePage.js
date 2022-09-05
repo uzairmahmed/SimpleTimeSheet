9bb68d21749ac85005c2ade7c98d074f9410a515
Uzair Ahmed <uzairmahmed@gmail.com>
2022-09-04 22:35:39 -0400

    css time :(

--- timesheets/src/pages/HomePage.js
+++ timesheets/src/pages/HomePage.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 
-import { Center, SimpleGrid, HStack, Flex, Spacer, VStack, Text, Button } from '@chakra-ui/react'
+import { Center, SimpleGrid, HStack, Flex, Spacer, VStack, Text, Button, Image } from '@chakra-ui/react'
 
 import UserCard from '../components/UserCard';
 
@@ -22,10 +22,12 @@ export default function UserPage(props) {
 
     return (
         <Flex direction={'column'} h='100vh'>
-            <HStack w="100%" bg='black' justify={'space-between'}>
-                <Center p='25' bg='green'></Center>
-                <Center p='25' bg='red'></Center>
-                <Center p='25' bg='blue'></Center>
+            <HStack w="100%" justify={'space-between'}>
+                <Image src='https://i.gyazo.com/d74f92946aa2ebf071b955c824a1b64c.png' m={25} w='250px' alt='Logo' />
+                <Center p='25'></Center>
+                <Center p='25'>
+                    <Button onClick={() => props.navigatePage(1)} colorScheme='blue'>Admin</Button>
+                </Center>
             </HStack>
 
             <Spacer />
